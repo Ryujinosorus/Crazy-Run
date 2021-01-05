d280ef94db578dc9c1bc22906598d77de81d7adb
Ryujinosorus <naim59950@gmail.com>
2021-01-05 15:16:33 +0100

    add

---
+++ Assets/SetScoreValue.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetScoreValue : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Text>().text = PlayerPrefs.GetString("hs_crazyrunner","0");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
